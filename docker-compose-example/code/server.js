734943bd423d8ff8f97ace7d743835a053b07c48
Richard Memory <richardfmemory@gmail.com>
2019-03-29 16:33:54 -0600

    Initial commit

---
+++ docker-compose-example/code/server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Docker + Node</h1>
+    <h2>A match made in the cloud</h2>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
---
+++ docker-compose-example/scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+npm install --save express
+npm install --save-dev nodemon
+npm start
